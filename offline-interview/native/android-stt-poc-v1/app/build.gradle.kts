e31e77d0ad68a05629b51cea40bb8ac34a17fc13
Stef <104558808+stefm78@users.noreply.github.com>
2026-09-11 08:48:22 +0200

    Add native Android STT POC app build

---
+++ offline-interview/native/android-stt-poc-v1/app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.stefm78.offlineinterview.nativepoc"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.stefm78.offlineinterview.nativepoc"
+        minSdk = 33
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
